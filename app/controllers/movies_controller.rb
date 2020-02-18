1af6ada8533ebdde49050e6b9beb2cb064f6ff69
srichalla66 <srichalla66@gmail.com>
2020-02-18 21:29:49 +0000

    part 2 debug

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = Moive.all_ratings
+    @all_ratings = ['G','PG','PG-13','R']
     
   #  if params[:ratings]
   #    @movies = Movie.where(rating: ratings)
