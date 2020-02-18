1872a174ae35009734e0cbb0ef6c85dde8800f21
srichalla66 <srichalla66@gmail.com>
2020-02-18 21:56:51 +0000

    part 2 debug

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,7 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = ['G','PG','PG-13','R']
-    @movies = Movie.where(rating: params[:ratings])
+    @movies = Movie.where(rating: ratings)
   #  if params[:ratings]
   #    @movies = Movie.where(rating: ratings)
   #  else
